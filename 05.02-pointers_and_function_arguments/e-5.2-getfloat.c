66bfeb13c3179c3003b07e61d3f5652de5f16577
nbnbnb <nbnbnb.zhang@gmail.com>
2022-01-21 09:22:45 +0800

    cc

--- 05.02-pointers_and_function_arguments/e-5.2-getfloat.c
+++ 05.02-pointers_and_function_arguments/e-5.2-getfloat.c
@@ -2,6 +2,13 @@
    of zero. Fix it to push such a character back to the input. */
 #include <stdio.h>
 
+// 5.2 指针与函数参数
+
+// 输入：12.34 4.5 6.7
+// 输入：回车
+// 输入：Ctrl+Z（Linux Ctrl+D）
+// 输出：12.34 4.5 6.7
+
 #define MAXLEN 1000
 
 int getint(float *);
@@ -13,9 +20,14 @@ int main(void)
 
   for (n = 0; n < MAXLEN && getint(&a[n]) != EOF; n++)
     ;
+
   printf("\n");
+
   for (int i = 0; i < n; i++)
+  {
     printf("%g ", a[i]);
+  }
+
   printf("\n");
   return 0;
 }
@@ -23,51 +35,38 @@ int main(void)
 #include <ctype.h>
 #include <math.h>
 
-int getch(void);
-void ungetch(int);
-
 /* getint: get next integer from input into *pn */
 int getint(float *pn)
 {
   int c, sign, prev = 0, j;
   float inte, frac;
 
-  while (isspace(c = getch())) // skip whitespaces
+  while (isspace(c = getchar())) // skip whitespaces
     ;
-  if (!isdigit(c) && c != EOF && c!= '+' && c != '-')
-    ungetch(c);   // it's not a number
+
   sign = (c == '-') ? -1 : 1;
+
   if (c == '+' || c == '-')
-    c = getch();
-  for (inte = 0; isdigit(c); c = getch())
-    inte = 10.0 * inte + (float) (c - '0');
+  {
+    c = getchar();
+  }
+
+  for (inte = 0; isdigit(c); c = getchar())
+  {
+    inte = 10.0 * inte + (float)(c - '0');
+  }
+
   if (c == '.')
+  {
+    c = getchar();
+
+    for (frac = 0, j = 0; isdigit(c); c = getchar(), j++)
     {
-      c = getch();
-      for (frac = 0, j = 0; isdigit(c); c = getch(), j++)
-	frac = 10.0 * frac + (float) (c - '0');
+      frac = 10.0 * frac + (float)(c - '0');
     }
-  *pn = ((float) sign) * (inte + frac/powf(10, j));
-  if (c != EOF)
-    ungetch(c);
+  }
+
+  *pn = ((float)sign) * (inte + frac / powf(10, j));
+
   return c;
 }
-
-#define BUFFSIZE 100
-
-char buf[BUFFSIZE];  // buffer for ungetch
-int bufp = 0;        // next free position in buf
-
-int getch(void)      // get a (possibly pushed back) character
-{
-  return (bufp > 0) ? buf[--bufp] : getchar();
-}
-
-void ungetch(int c)  // push back on input
-{
-  if (bufp >= BUFFSIZE)
-    printf("ungetch: too many characters\n");
-  else buf[bufp++] = c;
-}
-
-
--- 05.02-pointers_and_function_arguments/ex-getint.c
+++ 05.02-pointers_and_function_arguments/ex-getint.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+// 输入：1 23 4 5
+// 输入：回车
+// 输入：Ctrl+Z（Linux Ctrl+D）
+// 输出：1 23 4 5
+
 #define MAXLEN 1000
 
 int getint(int *);
@@ -8,59 +13,45 @@ int main(void)
 {
   int a[MAXLEN], n;
 
+  //  &a[n] 传递的是指针
   for (n = 0; n < MAXLEN && getint(&a[n]) != EOF; n++)
     ;
+
   printf("\n");
-  printf("%d", a[0]);
-  for (int i = 1; i < n; i++)
+
+  for (int i = 0; i < n; i++)
+  {
     printf("%d ", a[i]);
+  }
+
   printf("\n");
+
   return 0;
 }
 
 #include <ctype.h>
-
-int getch(void);
-void ungetch(int);
 
 /* getint: get next integer from input into *pn */
 int getint(int *pn)
 {
   int c, sign;
 
-  while (isspace(c = getch())) // skip whitespaces
+  while (isspace(c = getchar())) // skip whitespaces
     ;
-  if (!isdigit(c) && c != EOF && c!= '+' && c != '-')
-    {
-      ungetch(c);   // it's not a number
-      return 0;
-    }
+
   sign = (c == '-') ? -1 : 1;
+
   if (c == '+' || c == '-')
-    c = getch();
-  for (*pn = 0; isdigit(c); c = getch())
+  {
+    c = getchar();
+  }
+
+  for (*pn = 0; isdigit(c); c = getchar())
+  {
     *pn = 10 * *pn + (c - '0');
+  }
+
   *pn *= sign;
-  if (c != EOF)
-    ungetch(c);
+
   return c;
 }
-
-#define BUFFSIZE 100
-
-char buf[BUFFSIZE];  // buffer for ungetch
-int bufp = 0;        // next free position in buf
-
-int getch(void)      // get a (possibly pushed back) character
-{
-  return (bufp > 0) ? buf[--bufp] : getchar();
-}
-
-void ungetch(int c)  // push back on input
-{
-  if (bufp >= BUFFSIZE)
-    printf("ungetch: too many characters\n");
-  else buf[bufp++] = c;
-}
-
-
